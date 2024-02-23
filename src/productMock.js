89bff2676bc15dd5af48b03cbea366c410090b39
GonzaloBounzerio <bounzeriog@gmail.com>
2024-02-23 12:09:23 -0300

    cambios

--- src/productMock.js
+++ src/productMock.js
@@ -1,10 +1,10 @@
 export let products = [
   {
     id:1,
-    title:"Nos siguen pegando abajo",
+    title:"Piano Bar",
     autor:"Charly García",
-    año:9999,
-    price:123,
+    año:1984,
+    price:300,
     img:"",
     stock:123,
     category:"",
